a63a6145b9daa747f645313df6f4e68bd24b87f0
Breye Foka <breyefokalagloire@gmail.com>
2024-11-20 03:55:28 +0100

    updated the button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { View, StyleSheet } from 'react-native';
  import { Image } from 'expo-image'; 
 import ImageViewer from '../components/ImageViewer';
-
+import Button from '../components/button';
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 
@@ -11,7 +11,12 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage}/>
       </View>
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="Choose a photo" />
+        <Button label="Choose a Picture" />
+      </View>
     </View>
+
   );
 }
 
@@ -23,5 +28,9 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
